61b52e0a50cf4461761bf994d49db3aca243b9e9
Jaime <jaime.pena@in2.es>
2019-03-12 18:09:16 +0100

    TEST
    Test commit

--- Assets/BaseMovement.cs
+++ Assets/BaseMovement.cs
@@ -16,7 +16,6 @@ public abstract class BaseMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
